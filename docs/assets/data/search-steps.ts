b7fd76562fc83552d5fa3bf4d79490c691a06a69
sudi <112247156+iosudi@users.noreply.github.com>
2024-09-03 22:43:33 +0200

    deployment

---
+++ docs/assets/data/search-steps.ts
@@ -0,0 +1,124 @@
+export const unitTypes = [
+  {
+    icon: './assets/imgs/icons/studio.png',
+    title: 'استوديو',
+  },
+  {
+    icon: './assets/imgs/icons/apartment.png',
+    title: 'شقة',
+  },
+  {
+    icon: './assets/imgs/icons/villa.png',
+    title: 'فلة',
+  },
+];
+
+export const residentTypes = [
+  {
+    icon: './assets/imgs/icons/family.png',
+    title: 'أسرة',
+  },
+  {
+    icon: './assets/imgs/icons/man.png',
+    title: 'طالب',
+  },
+  {
+    icon: './assets/imgs/icons/woman.png',
+    title: 'طالبة',
+  },
+  {
+    icon: './assets/imgs/icons/bag.png',
+    title: 'موظف',
+  },
+];
+
+export const furnitureStates = [
+  {
+    icon: './assets/imgs/icons/Furnished.png',
+    title: 'مفروش',
+  },
+  {
+    icon: './assets/imgs/icons/Unfurnished.png',
+    title: 'غير مفروش',
+  },
+];
+
+export const rooms = [
+  {
+    title: 'السراير',
+    rooms_number_selection: [
+      {
+        rooms_number: 'الكل',
+      },
+      {
+        rooms_number: 1,
+      },
+      {
+        rooms_number: 2,
+      },
+      {
+        rooms_number: 3,
+      },
+      {
+        rooms_number: 4,
+      },
+      {
+        rooms_number: 5,
+      },
+      {
+        rooms_number: 6,
+      },
+      {
+        rooms_number: '6+',
+      },
+    ],
+  },
+
+  {
+    title: 'اوض النوم',
+    rooms_number_selection: [
+      {
+        rooms_number: 'الكل',
+      },
+      {
+        rooms_number: 1,
+      },
+      {
+        rooms_number: 2,
+      },
+      {
+        rooms_number: 3,
+      },
+      {
+        rooms_number: 4,
+      },
+      {
+        rooms_number: 5,
+      },
+      {
+        rooms_number: '5+',
+      },
+    ],
+  },
+
+  {
+    title: 'الحمام',
+    rooms_number_selection: [
+      {
+        rooms_number: 'الكل',
+      },
+      {
+        rooms_number: 1,
+      },
+      {
+        rooms_number: 2,
+      },
+      {
+        rooms_number: 3,
+      },
+      {
+        rooms_number: '3+',
+      },
+    ],
+  },
+];
---
+++ docs/assets/data/testimonials.ts
@@ -0,0 +1,26 @@
+export const Testimonials = [
+  {
+    content:
+      'أوصي بشدة بفريق JPS. لقد كانت الخدمة ممتازة واهتموا بكل التفاصيل لتلبية احتياجاتنا. نحن سعداء جدًا بمنزلنا الجديد!',
+    author: 'Barbara D. Smith',
+    profile_icon: './assets/imgs/7.png',
+  },
+  {
+    content:
+      'كانت تجربتنا مع JPS رائعة. محترفون ومتعاونون للغاية. قدموا لنا العديد من الخيارات حتى وجدنا ما يناسبنا تمامًا.',
+    author: 'Ahmed Al-Farsi',
+    profile_icon: './assets/imgs/8.png',
+  },
+  {
+    content:
+      'JPS قدمت لنا خدمة رائعة من البداية حتى النهاية. كانت عملية البحث عن المنزل سهلة وسلسة بفضلهم.',
+    author: 'Sara M. Johnson',
+    profile_icon: './assets/imgs/2.png',
+  },
+  {
+    content:
+      'شكرًا لـ JPS على العمل الرائع. كانوا دائمًا متاحين للإجابة على أسئلتنا ومساعدتنا في كل خطوة من العملية.',
+    author: 'Michael R. Lee',
+    profile_icon: './assets/imgs/3.png',
+  },
+];
